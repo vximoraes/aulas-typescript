f4abfefff8cde48533bbadd1deb7ad4cc42c777a
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2024-11-04 19:51:08 -0400

    date

--- date/src/index.ts
+++ date/src/index.ts
@@ -3,3 +3,11 @@ console.log(hoje)
 
 console.log(hoje.toString())
 console.log(hoje.toUTCString()) 
+
+console.log(`Hoje: ${hoje.getDate()}/${hoje.getMonth() + 1}/${hoje.getFullYear()}`)
+
+let dataNascimento: Date = new Date("2005-06-23")
+console.log(dataNascimento)
+
+let dataAtualizacaoDoSoftware: Date = new Date("2021-06-23T10:30:00")
+console.log(dataAtualizacaoDoSoftware.toString())
