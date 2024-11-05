90e80da22ddce947572fdc7f696767b57c35bcca
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2024-11-04 20:40:51 -0400

    adiciona interface Estudante e enums para Logradouro e UF

---
+++ date/README.md
@@ -0,0 +1,7 @@
+Criar um objeto Estudante com as seguintes características:
+- Nome [mínimo de 3 caracteres e máximo de 50 caracteres]
+- Endereço [sub-objeto?] [tipo de logradouro (enum?), logradouro, número, bairro]
+- Cidade [array?]
+- UF [array?]
+- Data de nascimento [somente data]
+- Data de cadastro [data atual]
--- date/src/index.ts
+++ date/src/index.ts
@@ -1,13 +1,87 @@
-let hoje: Date = new Date()
-console.log(hoje)
+enum Logradouro {
+    Alameda    = 'Alameda',
+    Avenida    = 'Avenida',
+    Campo      = 'Campo',
+    Chácara    = 'Chácara',
+    Colônia    = 'Colônia',
+    Condomínio = 'Condomínio',
+    Conjunto   = 'Conjunto',
+    Distrito   = 'Distrito',
+    Esplanada  = 'Esplanada',
+    Estação    = 'Estação'
+}
 
-console.log(hoje.toString())
-console.log(hoje.toUTCString()) 
+enum Uf{
+    AC = 'Acre',
+    AL = 'Alagoas',
+    AP = 'Amapá',
+    AM = 'Amazonas',
+    BA = 'Bahia',
+    CE = 'Ceará',
+    DF = 'Distrito Federal',
+    ES = 'Espírito Santo',
+    GO = 'Goiás',
+    MA = 'Maranhão',
+    MT = 'Mato Grosso',
+    MS = 'Mato Grosso do Sul',
+    MG = 'Minas Gerais',
+    PA = 'Pará',
+    PB = 'Paraíba',
+    PR = 'Paraná',
+    PE = 'Pernambuco',
+    PI = 'Piauí',
+    RJ = 'Rio de Janeiro',
+    RN = 'Rio Grande do Norte',
+    RS = 'Rio Grande do Sul',
+    RO = 'Rondônia',
+    RR = 'Roraima',
+    SC = 'Santa Catarina',
+    SP = 'São Paulo',
+    SE = 'Sergipe',
+    TO = 'Tocantins'
+}
 
-console.log(`Hoje: ${hoje.getDate()}/${hoje.getMonth() + 1}/${hoje.getFullYear()}`)
+interface Cidade {
+    nome: string,
+    estado: Uf
+}
 
-let dataNascimento: Date = new Date("2005-06-23")
-console.log(dataNascimento)
+let vilhena: Cidade = {
+    nome: 'Vilhena',
+    estado: Uf.RO
+}
 
-let dataAtualizacaoDoSoftware: Date = new Date("2021-06-23T10:30:00")
-console.log(dataAtualizacaoDoSoftware.toString())
+interface Estudante {
+    nome: string,
+    endereco?: {
+        logradouro: Logradouro,
+        numero: number,
+        bairro: string
+    },
+    cidade?: Cidade,
+    UF?: Uf,
+    dataNascimento: Date,
+    dataDeCadastro: Date
+}
+
+let estudante: Estudante = {
+    nome: 'Vinícius Almeida Moraes',
+    endereco: {
+        logradouro: Logradouro.Avenida,
+        numero: 3840,
+        bairro: 'Centro'
+    },
+    cidade: vilhena,
+    UF: Uf.RO,
+    dataNascimento: new Date('2005-06-23'),
+    dataDeCadastro: new Date('2024-11-04')
+}
+
+console.log(`Nome: ${estudante.nome}
+Logradouro: ${estudante.endereco?.logradouro}
+Número: ${estudante.endereco?.numero}
+Bairro: ${estudante.endereco?.bairro}
+Cidade: ${estudante.cidade?.nome}
+UF: ${estudante.UF}
+Data de Nascimento: ${estudante.dataNascimento.getDate() + 1}/${estudante.dataNascimento.getMonth() + 1}/${estudante.dataNascimento.getFullYear()}
+Data de Cadastro: ${estudante.dataDeCadastro.getDate() + 1}/${estudante.dataDeCadastro.getMonth() + 1}/${estudante.dataDeCadastro.getFullYear()}`)
