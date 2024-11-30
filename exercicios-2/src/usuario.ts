3c6009b7860984a8eea744447bfd87b576290be1
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2024-11-29 23:24:42 -0400

    detalhes usuario

--- exercicios-2/src/usuario.ts
+++ exercicios-2/src/usuario.ts
@@ -1,5 +1,5 @@
 import emailValidator from 'email-validator'
-import PasswordValidator from 'password-validator';
+import PasswordValidator from 'password-validator'
 
 // Definindo as regras para a senha.
 const validarSenha = new PasswordValidator()
@@ -18,29 +18,29 @@ interface Usuario {
 
 // Função para validar os usuários.
 function validarUsuario(usuario: Usuario): string[] {
-    let mensagens: string[] = [];
+    let mensagens: string[] = []
 
     // Validação do username.
     if (usuario.username.length < 5) {
-        mensagens.push('O username deve conter no mínimo 5 caracteres');
+        mensagens.push('O username deve conter no mínimo 5 caracteres')
     }
 
     // Validação do email.
     if (!emailValidator.validate(usuario.email)) {
-        mensagens.push('O email deve ser um endereço de e-mail válido');
+        mensagens.push('O email deve ser um endereço de e-mail válido')
     }
 
     // Validação da senha.
     if (!validarSenha.validate(usuario.password)) {
-        mensagens.push('A senha deve conter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula e um número.');
+        mensagens.push('A senha deve conter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula e um número.')
     }
 
     // Validação da idade.
     if (usuario.idade < 18) {
-        mensagens.push('A idade deve ser maior ou igual a 18 anos.');
+        mensagens.push('A idade deve ser maior ou igual a 18 anos.')
     }
 
-    return mensagens;
+    return mensagens
 }
 
 // Criando usuários.
